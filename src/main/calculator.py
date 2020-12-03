18500b7a76de91465c52ddc620fc20df80c84109
frankZawacki <frank.zawacki@snhu.edu>
2020-12-03 11:39:12 -0500

    update calculator perscholas_rocks and predicator.

--- src/main/calculator.py
+++ src/main/calculator.py
@@ -1,13 +1,16 @@
 # Created by Leon Hunter at 9:54 AM 10/23/2020
 class Calculator(object):
     def add(self, a, b):
-        return None # TODO - Implement solution
+        return a + b
 
     def subtract(self, a, b):
-        return None # TODO - Implement solution
+        return a - b
 
     def multiply(self, a, b):
-        return None # TODO - Implement solution
+        return a * b
 
     def divide(self, a, b):
-        return None # TODO - Implement solution
+        if (b != 0)
+            return a / b
+        else:
+            return ZeroDivisionError
--- src/main/string_evaluator.py
+++ src/main/string_evaluator.py
@@ -1,7 +1,7 @@
 # Created by Leon Hunter at 3:57 PM 10/23/2020
 class StringManipulator(object):
     def get_hello_world(self):
-        return None # TODO - Implement solution
+        return "Hello World"
 
     def concatenate(self, value_to_be_added_to, value_to_add):
         return None # TODO - Implement solution
